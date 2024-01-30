be967856d0f88960bc3753d7b6a83ac7d3aa7e47
magiof <tjddnjs7559@gamil.com>
2024-01-31 01:46:02 +0900

    chore: RolesGuard 적용

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,11 +4,13 @@ import { AppService } from './app.service';
 import { DBModule } from './db/db.module';
 import { ContextModule } from './context/context.module';
 import { InjectAccountMiddleware } from './middlewares/InjectAccount.middleware';
+import { APP_GUARD } from '@nestjs/core';
+import { RolesGuard } from './guards/roles.guard';
 
 @Module({
   imports: [DBModule, ContextModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, { provide: APP_GUARD, useClass: RolesGuard }],
 })
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
