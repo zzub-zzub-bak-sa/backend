9efc0034117959bb046d20f7e2424c42df33b2b7
su-hwani <wjdtnghks123@kookmin.ac.kr>
2024-03-20 18:48:47 +0900

    update: folder name change

--- src/context/account/users/users.controller.ts
+++ src/context/account/users/users.controller.ts
@@ -11,7 +11,7 @@ export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Get('test') test(@Query('withError') withError?: boolean) {
-    const test_version = 'UpdatedAt: 2024.03.19';
+    const test_version = 'UpdatedAt: 2024.03.20';
     return test_version;
   }
 
