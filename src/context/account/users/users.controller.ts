434012f6ba108b31393710caf26f8e7bfc6d1d8d
su-hwani <wjdtnghks123@kookmin.ac.kr>
2024-03-20 23:36:00 +0900

    deploy: version 2024.03.20 (23:28) only docker

--- src/context/account/users/users.controller.ts
+++ src/context/account/users/users.controller.ts
@@ -11,7 +11,7 @@ export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Get('test') test(@Query('withError') withError?: boolean) {
-    const test_version = 'UpdatedAt: 2024.03.21 (23:28)';
+    const test_version = 'UpdatedAt: 2024.03.21 (23:35)';
     return test_version;
   }
 
